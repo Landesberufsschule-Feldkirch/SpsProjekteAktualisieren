2380b474becdef63a81c9affd852a1e1c36d6af7
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-05-13 17:34:48 +0200

    Änderungen vom Labor

---
+++ SpsProjekteAktualisieren/SpsProjekteAktualisieren/Model/DateiListe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace SpsProjekteAktualisieren.Model
+{
+
+    public class DateiListe
+    {
+        public ObservableCollection<DateiEigenschaften> AlleDateien { get; set; } = new ObservableCollection<DateiEigenschaften>();
+    }
+
+    public class DateiEigenschaften
+    {
+        public string Beschreibung { get; set; }
+        public string Dateiname { get; set; }
+
+       
+
+
+        public DateiEigenschaften()
+        {
+            Beschreibung = "";
+            Dateiname = "";
+        }
+
+    }
+}
--- SpsProjekteAktualisieren/SpsProjekteAktualisieren/Model/ProjekteAktualsieren.cs
+++ SpsProjekteAktualisieren/SpsProjekteAktualisieren/Model/ProjekteAktualsieren.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Windows;
 
 namespace SpsProjekteAktualisieren.Model
 {
     public class ProjekteAktualsieren
     {
+        public DateiListe AlleDateiListen { get; set; }
         public ProjektVerzeichnisse OrdnerStruktur { get; set; }
         public string QuellOrdner { get; set; }
         public string ZielOrdner { get; set; }
@@ -16,8 +15,12 @@ namespace SpsProjekteAktualisieren.Model
         private readonly StringBuilder _textBoxText;
         private System.Collections.Generic.IEnumerable<string> _fileNames;
 
+        const int BYTES_TO_READ = sizeof(Int64);
+
         public ProjekteAktualsieren()
         {
+            AlleDateiListen = Newtonsoft.Json.JsonConvert.DeserializeObject<DateiListe>(File.ReadAllText("DateiListen.json"));
+
             _textBoxText = new StringBuilder();
             OrdnerStruktur = Newtonsoft.Json.JsonConvert.DeserializeObject<ProjektVerzeichnisse>(File.ReadAllText("ProjektVerzeichnisse.json"));
             if (OrdnerStruktur.AlleProjektVerzeichnisse[0].Kommentar == "Ordner")
@@ -30,8 +33,19 @@ namespace SpsProjekteAktualisieren.Model
             foreach (var struktur in OrdnerStruktur.AlleProjektVerzeichnisse)
             {
                 if (struktur.Kommentar == "Ordner") continue;
-                _textBoxText.Append(QuellOrdner + "/" + struktur.Quelle + "\n");
-                _textBoxText.Append(ZielOrdner + "/" + struktur.Ziel + "\n\n");
+                var Quelle = QuellOrdner + "/" + struktur.Quelle;
+                var Ziel = ZielOrdner + "/" + struktur.Ziel;
+
+                foreach (var datei in AlleDateiListen.AlleDateien)
+                {
+                    if (!FilesAreEqual(new FileInfo(Quelle + "/" + datei.Dateiname), new FileInfo(datei.Dateiname)))
+                    {
+                        _textBoxText.Clear();
+                        _textBoxText.Append("IP Adressen sind unterschiedlich eingestellt! \n");
+                    }
+                    _textBoxText.Append(Quelle + "\n");
+                    _textBoxText.Append(Ziel + "\n\n");
+                }
             }
         }
 
@@ -99,5 +113,35 @@ namespace SpsProjekteAktualisieren.Model
             }
         }
         internal StringBuilder TextBoxText() => _textBoxText;
+
+
+
+        static public bool FilesAreEqual(FileInfo first, FileInfo second)
+        {
+            if (first.Length != second.Length)
+                return false;
+
+            if (string.Equals(first.FullName, second.FullName, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            int iterations = (int)Math.Ceiling((double)first.Length / BYTES_TO_READ);
+
+            using (FileStream fs1 = first.OpenRead())
+            using (FileStream fs2 = second.OpenRead())
+            {
+                byte[] one = new byte[BYTES_TO_READ];
+                byte[] two = new byte[BYTES_TO_READ];
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    fs1.Read(one, 0, BYTES_TO_READ);
+                    fs2.Read(two, 0, BYTES_TO_READ);
+
+                    if (BitConverter.ToInt64(one, 0) != BitConverter.ToInt64(two, 0)) return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
